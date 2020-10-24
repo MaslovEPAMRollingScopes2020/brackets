c91abc72287d75f1460ca9d1a07eefddd68276b0
MaslovEPAMRollingScopes2020 <epamrollingscopes@gmail.com>
2020-10-24 15:53:29 +0300

    init: start task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let flag = true;
+  let findingPosition = -1;
+  let i;
+  for(; str.length > 0;){
+    
+    for(i = 0; i < bracketsConfig.length; i++){
+      findingPosition = str.indexOf(bracketsConfig[i].join(''));
+      if(findingPosition !== -1){
+        break;
+      }
+    }
+
+    if(findingPosition === -1){
+      return false;
+    }
+
+    str = str.split('');
+    str.splice(findingPosition, 2);
+    str = str.join('');
+  }
+
+  return true;
 }
